e8b77286991ce5d705b4e14d16cc3d8222b8399b
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-13 04:47:54 +0900

    text editor database file done with put and get request

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  const editorDb = await openDB('text', 1);
+
+  const tx = editorDb.transaction('text', 'readwrite');
+
+  const store = tx.objectStore('text');
+
+  const request = store.add({ content: content });
+
+  const result = await request;
+
+console.log('data put request to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log ('GET from the database');
+
+  const editorDb = await openDB('text', 1);
+
+  const tx = editorDb.transaction('text', 'readonly');
+
+  const store = tx.objectStore('text');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,11 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+
+
+    
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
