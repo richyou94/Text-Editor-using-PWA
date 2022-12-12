77752d2369c24700ca427d94f862b781cd826b5e
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-12-13 05:07:21 +0900

    done with webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -50,7 +50,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-propoosal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
